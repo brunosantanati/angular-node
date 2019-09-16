a1bdbf8b75f65c9a973d49aa3cfa9f50d86b2efd
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2019-09-16 13:54:40 -0300

    Consuming data from API and displaying it in the page

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-posts',
   templateUrl: './posts.component.html',
-  styleUrls: ['./posts.component.css']
+  styleUrls: ['./posts.component.css'],
+  providers: [PostsService]
 })
 export class PostsComponent implements OnInit {
 
-  constructor() { }
+  posts: any = [];
+
+  constructor(private postService: PostsService) { }
 
   ngOnInit() {
+    this.postService.getAllPosts().subscribe(posts => {
+      this.posts = posts;
+    });
   }
 
 }
