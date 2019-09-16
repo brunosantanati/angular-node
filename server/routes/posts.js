0d73d91a05568137a920fedc7e92f9b3288b122e
Bruno Sant' Ana <bruno.santana.ti@gmail.com>
2019-09-16 11:01:09 -0300

    Make the endpoint return some json fake data

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+
+const postAPI = 'https://jsonplaceholder.typicode.com';
 
 // GET POST
 router.get('/', (req, res) => {
-    res.send('POST WORKS');
+    
+    //res.send('POST WORKS');
+
+    axios.get(`${postAPI}/posts`).then(posts => {
+        console.log(posts.data);
+    });
+
 });
 
 module.exports = router;
